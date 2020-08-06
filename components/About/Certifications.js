76aecd0a501b65fff72a987ed5f377361dd098f5
Daine <hello@dainemawer.com>
2020-08-06 15:25:33 +0200

    🔮 add static pages

---
+++ components/About/Certifications.js
@@ -0,0 +1,36 @@
+/**
+ * 🔖 Certification Component
+ *
+ * Outputs About Certifications
+ *
+ * @author Daine Mawer
+ * @version 1.0
+ *
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './About.module.scss'
+
+const Certifications = ({ title }) => {
+    return (
+        <section className={styles.section}>
+            {title && <h3 className={styles.title}>{title}</h3>}
+            <ul className={styles.list}>
+                <li className={styles['certification-item']}>CS50 - Introduction to Computer Science | <a className={styles['certification-link']} href="https://www.edx.org/course/cs50s-introduction-to-computer-science" rel="noopener noreferrer">Link</a></li>
+                <li className={styles['certification-item']}>AWS Certified Developer | <a className={styles['certification-link']} href="https://aws.amazon.com/certification/certified-developer-associate/" rel="noopener noreferrer">Link</a></li>
+                <li className={styles['certification-item']}>Google Cloud Certificed Developer | <a className={styles['certification-link']} href="https://cloud.google.com/certification/cloud-developer" rel="noopener noreferrer">Link</a></li>
+                <li className={styles['certification-item']}>Certified Agile42 Scrum Master | <a className={styles['certification-link']} href="https://www.agile42.com/en/csm-certified-scrum-master-training/" rel="noopener noreferrer">Link</a></li>
+            </ul>
+        </section>
+    );
+}
+
+Certifications.propTypes = {
+    title: PropTypes.string,
+}
+
+Certifications.defaultProps = {
+    title: '',
+}
+
+export default Certifications;
---
+++ components/About/Content.js
@@ -0,0 +1,32 @@
+/**
+ * 🧑‍💻 About Content
+ *
+ * Handles special layout on About template
+ *
+ * @author Daine Mawer
+ * @version 1.0
+ *
+ */
+import React from 'react';
+import styles from './About.module.scss'
+
+const Content = () => {
+    return (
+        <>
+            <section className={styles.grid}>
+                <div>
+                    <img className={styles.image} src="/dainemawer.jpg" alt="Daine Mawer" />
+                </div>
+                <div>
+                    <p>Ramps banh mi chillwave readymade bushwick, pug hella glossier palo santo meggings photo booth. Tattooed dreamcatcher mixtape ennui, tbh leggings drinking vinegar. Fam banh mi chartreuse banjo live-edge. Selvage woke poutine, literally thundercats you probably haven't heard of them everyday carry wolf 3 wolf moon. Meggings literally tilde humblebrag keffiyeh, blue bottle kinfolk flannel synth cred sriracha.</p>
+                    <p>90's biodiesel man braid taxidermy woke. Iceland literally air plant, tousled letterpress bicycle rights trust fund enamel pin tote bag YOLO vaporware umami everyday carry jianbing hammock. Messenger bag cred sriracha art party four dollar toast fam. Meggings literally tilde humblebrag keffiyeh, blue bottle kinfolk flannel synth cred sriracha. Iceland literally air plant, tousled letterpress bicycle rights.</p>
+                </div>
+            </section>
+            <div>
+                <p>Messenger bag fingerstache poke organic pork belly tilde literally banh mi master cleanse kitsch. Chartreuse retro jianbing neutra artisan, fanny pack tumeric small batch stumptown hot chicken cloud bread occupy vice. Fingerstache 8-bit fanny pack occupy four dollar toast af mustache 3 wolf moon. Af street art twee hashtag slow-carb.</p>
+            </div>
+        </>
+    );
+}
+
+export default Content;
