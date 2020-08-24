e78e8eda774c56097731da622472f1f1475bd49e
Daine <hello@dainemawer.com>
2020-08-24 06:33:46 +0200

    🌕Add DarkMode functionality

---
+++ context/context.js
@@ -0,0 +1,16 @@
+/**
+ * 💡 React Context API
+ *
+ * Inits React Context
+ *
+ * @file context
+ * @author Daine Mawer
+ * @since 1.0.0
+ */
+
+// 📦 NPM
+import { createContext } from 'react';
+
+const Context = createContext();
+
+export default Context;
